e90f5c6b7519052ca82b514e72180c31ce0d2bbb
Bilal Ashraf <mr.bilal4972@gmail.com>
2020-04-12 12:33:01 +0500

    String Assignment

---
+++ String/app.js
@@ -0,0 +1,11 @@
+let str1 = "";
+
+str1 = `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam
+velit neque, ratione veritatis eveniet ullam voluptatem quis
+distinctio minus sapiente deserunt impedit, dolorum quas
+obcaecati eum tempore reiciendis placeat atque eligendi aperiam
+dicta vero incidunt quisquam. Natus eos accusantium facere,
+officiis atque, blanditiis minima, cum optio velit eius ea
+odit!`
+
+console.log(str1);
