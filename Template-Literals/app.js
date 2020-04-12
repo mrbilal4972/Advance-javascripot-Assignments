07223a5a68076806e054932f7be89df6f505a9b2
Bilal Ashraf <mr.bilal4972@gmail.com>
2020-04-12 11:20:00 +0500

    Template Literal Assignment

---
+++ Template-Literals/app.js
@@ -0,0 +1,19 @@
+let uname = 'Bilal'
+let fname = 'Ashraf';
+let email = 'mr.bilal4972@gmail.com';
+
+let coverLetter = "";
+
+coverLetter = `${uname} ${fname}
+${email}
+
+
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam
+velit neque, ratione veritatis eveniet ullam voluptatem quis
+distinctio minus sapiente deserunt impedit, dolorum quas
+obcaecati eum tempore reiciendis placeat atque eligendi aperiam
+dicta vero incidunt quisquam. Natus eos accusantium facere,
+officiis atque, blanditiis minima, cum optio velit eius ea
+odit!`
+
+console.log(coverLetter);
